e515fa437a512eeb8ce776971ccea561fa6dde9c
Elad R. Katz <eladkt@gmail.com>
2017-03-02 13:08:55 +0200

    first commit

---
+++ spec/codewalker-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodewalkerView from '../lib/codewalker-view';
+
+describe('CodewalkerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
